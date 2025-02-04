fced12b0724972fb530292692e85b6bb6e13d622
Luan Bossardi <xdbilu@gmail.com>
2025-02-04 20:16:14 -0300

    Service layer, component registratoin

---
+++ src/main/java/com/projetospring/project/services/UserService.java
@@ -0,0 +1,29 @@
+package com.projetospring.project.services;
+
+
+import com.projetospring.project.entities.User;
+import com.projetospring.project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+
+    }
+
+    public User findById(Long id){
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+}
